70e144ff5cdbe33a5b67b856d77be479d0a5d54d
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-09-30 10:05:42 +0000

    Data Type Summary

---
+++ _1_Basic/_7_DataType-Summary.js
@@ -0,0 +1,62 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+
+let str = "Chetan";
+console.log(typeof str);
+
+let num = 23123;
+console.log(typeof num);
+
+let bool = "Chetan";
+console.log(typeof str);
+
+let nulll = null;
+console.log(typeof nulll);
+
+let undef = undefined;
+console.log(typeof undef);
+
+
+console.log(typeof heros);
+
+
+console.log(typeof myObj);
+
+
+console.log(typeof myFunction);
+
