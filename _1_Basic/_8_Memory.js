e5eda3759a839ca7d9ac1c5df2f61d266f2928f1
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-09-30 10:38:10 +0000

    Stack and Heap Memory Working

---
+++ _1_Basic/_8_Memory.js
@@ -0,0 +1,21 @@
+let name1 = "chetan Joshi";
+
+let name2 = name1;
+name2 = "Abhishek Joshi";
+
+console.log(name1);
+console.log(name2);
+
+let UserOne = {
+    Email:"chetan@gmail.com",
+    Password:33840012,
+};
+
+let UserTwo = UserOne;
+
+UserTwo.Email = "Abhishek Joshi";
+UserTwo.Password = 75880242;
+
+
+console.log(UserOne);
+console.log(UserTwo);
