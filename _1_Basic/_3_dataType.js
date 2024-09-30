0b71f939b4056086668a0d0665a51a003b2f531e
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-09-30 08:27:55 +0000

    data types

---
+++ _1_Basic/_3_dataType.js
@@ -0,0 +1,12 @@
+"use strict"  /* treat all js code as newer version */
+
+let name = "Chetan";
+let age = 21;
+let male = true;
+
+console.log(typeof age);
+
+console.log(typeof name);
+console.log(typeof male);
+console.log(typeof undifine);
+console.log(typeof null);
