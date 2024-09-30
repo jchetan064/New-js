2f24dff0f5285bc3bf302c67ffc8009c131dbfd1
jchetan064 <113768496+jchetan064@users.noreply.github.com>
2024-09-30 08:27:41 +0000

    variables

---
+++ _1_Basic/_2_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12233;
+var accountEmail = "cshhdhf112@gmail.com";
+let accountPassword = "337kcnsue902";
+
+accountCity = "Jaipur";
+
+let accountStatus;
+console.log(accountId);
+accountId == 287384;
+console.log(accountId);
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountStatus,
+]);
